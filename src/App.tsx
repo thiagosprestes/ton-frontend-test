4073b2eaf3f02bc68d68d40bbd47b9fbca8f8cd9
thiagosprestes <thiago.santos.prestes@gmail.com>
2021-03-11 18:57:57 -0300

    Adicionada persistencia de dados do carrinho.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import 'react-native-gesture-handler';
 
@@ -10,14 +11,16 @@ import 'intl/locale-data/jsonp/pt-BR';
 
 import Routes from './routes';
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#00bf00" />
       <Provider store={store}>
-        <Routes />
+        <PersistGate loading={null} persistor={persistor}>
+          <Routes />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,19 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import {createStore} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
 
 import {rootReducer} from './reducers';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['cart'],
+};
 
-export default store;
+const persistedReducer = persistReducer<any, any>(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+
+const persistor = persistStore(store);
+
+export {store, persistor};
